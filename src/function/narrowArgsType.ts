99c7b682ff0e0e6e3a52048685cc1fcac231d56f
myusuf963 <mohammad963yusuf@gmail.com>
2023-01-12 23:33:22 +0000

    add file

---
+++ src/function/narrowArgsType.ts
@@ -0,0 +1,18 @@
+const myFunc1 = <T>(a: T): T => a;
+
+myFunc1<string>("1");
+
+const myFunc2 = (...args: number[]) => {
+  if (args[0] !== undefined && args[1] !== undefined) {
+    return args[0] + args[1];
+  }
+  return 0;
+};
+const myFunc3 = (...args: number[]) => {
+  if (args.length > 0) {
+    return args.reduce((sum, num) => sum + num, 0);
+  }
+  return 0;
+};
+
+myFunc2(1, 2);
